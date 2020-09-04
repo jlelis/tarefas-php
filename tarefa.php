e0603952e0da203b87fdee6e6201e94bcc516988
Jeferson Lelis <jeferson.lelis@outlook.com>
2020-09-03 22:49:45 -0300

    first commit

---
+++ tarefa.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<h1>Gerenciador de Tarefas</h1>
+<!-- Aqui o Codigo-->
+
+<body>
+    <form action="">
+        <fieldset>
+            <legend>Nova tarefa</legend>
+            <label>Tarefa: </label>
+            <input type="text" name="nome" />
+            <input type="submit" value="Cadastrar" />
+
+        </fieldset>
+    </form>
+
+</body>
+
+</html>
